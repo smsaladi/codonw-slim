44364cb6bdf5daf21d74ccfc84b885335cb7ab84
Shyam Saladi <smsaladi@gmail.com>
2020-01-21 11:54:30 -0800

    refactor regression tests for blk files as well

--- test/test_regression.py
+++ test/test_regression.py
@@ -1,5 +1,5 @@
 """
-Quick regression test of codonw
+Codonw(-barebones) regression tests
 """
 
 import os
@@ -8,23 +8,28 @@ import subprocess
 import numpy as np
 import pandas as pd
 
+import pytest
+
 # location of *this* script
 path = os.path.dirname(os.path.realpath(__file__))
 
 with open(os.devnull, 'w') as devnull:
     # compile 
-    subprocess.check_call("make -C {}/../ clean && make -j4 -C {}/../".format(path, path),
+    subprocess.check_call("make -C {p}/../ clean && make -j4 -C {p}/../".format(p=path),
                           shell=True, stderr=devnull)
 
-    # run test file
-    subprocess.check_call(
-        "{}/../bin/codonw {}/input.dat test blk "
-        "-all_indices -aro -hyd -nomenu -silent -nowarn -dinuc -machine > {}/test.out".format(path, path, path),
-        shell=True, stderr=devnull)
 
-def test_regression():
+def test_index_regression():
+    out_fn = "{}/input.out".format(path)
+
+    with open(os.devnull, 'w') as devnull:
+        subprocess.check_call(
+            "{p}/../bin/codonw {p}/input.fna {o} /dev/null "
+            "-all_indices -aro -hyd -nomenu -silent -nowarn -noblk -machine".format(p=path, o=out_fn),
+            shell=True, stdout=devnull, stderr=devnull)
+            
     # read ref and test output
-    df_ref = pd.read_csv("{}/ref/out.txt".format(path), index_col="title",
+    df_ref = pd.read_csv(out_fn, index_col="title",
                          sep='[\\s,]+', engine='python')
     df_ref.index = df_ref.index.str.split(' ').str[0]
     df_ref.dropna(axis=1, inplace=True)
@@ -41,3 +46,21 @@ def test_regression():
     return
 
 
+@pytest.mark.parametrize("blktype", [
+    "dinuc",
+    "aau",
+    "raau",
+    "cu",
+    "cutab",
+    "cutot",
+    "rscu",
+    "base"
+])
+def test_bulk_regression(blktype):
+    with open(os.devnull, 'w') as devnull:
+        subprocess.check_call(
+            "{p}/../bin/codonw {p}/input.fna /dev/null {p}/{b}.blk "
+            "-nomenu -silent -nowarn -machine -{b}".format(p=path, b=blktype),
+            shell=True, stdout=devnull)
+
+    return
